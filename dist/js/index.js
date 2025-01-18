358414eb776fd0fbeb47d12e7467403e58dac70f
harshsahu383 <harshsahu1917@gmail.com>
2025-01-18 23:32:46 +0530

    skills section is added

--- dist/js/index.js
+++ dist/js/index.js
@@ -11,33 +11,35 @@ const dynamicText = document.getElementById('dynamic-text');
       const currentRole = roles[currentIndex];
 
       if (isDeleting) {
-        // Remove characters
+       
         dynamicText.textContent = currentRole.substring(0, charIndex - 1);
         charIndex--;
       } else {
-        // Add characters
+       
         dynamicText.textContent = currentRole.substring(0, charIndex + 1);
         charIndex++;
       }
 
-      // Adjust the horizontal line width
+      
       const textWidth = dynamicText.offsetWidth;
       line.style.width = textWidth + 'px';
 
-      // Handle completion of typing or deleting
+      
       if (!isDeleting && charIndex === currentRole.length) {
         isDeleting = true;
-        setTimeout(typeEffect, 1000); // Pause before deleting
+        setTimeout(typeEffect, 1000); 
         return;
       } else if (isDeleting && charIndex === 0) {
         isDeleting = false;
-        currentIndex = (currentIndex + 1) % roles.length; // Move to the next role
+        currentIndex = (currentIndex + 1) % roles.length; 
       }
 
-      // Adjust typing speed
-      const typingSpeed = isDeleting ? 100 : 150; // Faster when deleting
+     
+      const typingSpeed = isDeleting ? 100 : 150; 
       setTimeout(typeEffect, typingSpeed);
     }
 
-    // Start the typing effect
+    
     typeEffect();
+    // skills
+   
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       animation: {
         blink: 'blink 1s step-start infinite',
         floating: 'floating 3s ease-in-out infinite',
+        marquee: 'marquee 20s linear infinite',
       },
       keyframes: {
         blink: {
@@ -20,6 +21,10 @@ module.exports = {
         floating: {
           '0%, 100%': { transform: 'translateY(0)' }, // Start and end positions (no movement)
           '50%': { transform: 'translateY(-15px)' },  // Midway position (slightly up)
+        },
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
         },
       },
     },
